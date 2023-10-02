81c28e343318c0e3a361415c9c8834ebbb0b94de
Bot0FF <92510704+Bot0FF@users.noreply.github.com>
2023-10-02 22:26:58 +0300

    spring authentication

--- src/main/java/org/bot0ff/Application.java
+++ src/main/java/org/bot0ff/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import javax.sql.DataSource;
 import java.util.Map;
 
 import static org.springframework.web.servlet.function.RequestPredicates.GET;
@@ -23,44 +25,23 @@ public class Application {
         SpringApplication.run(Application.class);
     }
 
-//    //точка входа глобально
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf().disable()
-//                .formLogin().and()
-//                .authorizeHttpRequests()
-//                .requestMatchers("/public/**").permitAll()
-//                .anyRequest().authenticated().and()
-//                .exceptionHandling()
-//                .authenticationEntryPoint((request, response, authException) -> {
-//                    response.sendRedirect("http://localhost:8080/public/sign-in.html");
-//                }).and()
-//                .build();
-//    }
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsService(dataSource);
+    }
 
-    //точка входа сконфигурирована в фильтре
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        BasicAuthenticationEntryPoint basicAuthenticationEntryPoint = new BasicAuthenticationEntryPoint();
-        basicAuthenticationEntryPoint.setRealmName("Realm");
-
         return http
-                .httpBasic().authenticationEntryPoint(((request, response, authException) -> {
-                    authException.printStackTrace();
-                    basicAuthenticationEntryPoint.commence(request, response, authException);
-                })).and()
+                .httpBasic().and()
                 .authorizeHttpRequests()
                 .anyRequest().authenticated().and()
-                .exceptionHandling()
-                .authenticationEntryPoint(basicAuthenticationEntryPoint)
-                .and()
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
         return route(GET("/get"),
-                req -> ok().body(Map.of("id", "One")));
+                req -> ok().body(Map.of("Access", "Allowed")));
     }
 }
