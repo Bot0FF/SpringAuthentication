9b746bfdca344be007878abc5331cb06c739f154
Bot0FF <92510704+Bot0FF@users.noreply.github.com>
2023-10-03 23:16:43 +0300

    spring rest security

--- src/main/java/org/bot0ff/Application.java
+++ src/main/java/org/bot0ff/Application.java
@@ -2,46 +2,11 @@ package org.bot0ff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import javax.sql.DataSource;
-import java.util.Map;
-
-import static org.springframework.web.servlet.function.RequestPredicates.GET;
-import static org.springframework.web.servlet.function.RouterFunctions.route;
-import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-    }
-
-    @Bean
-    UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsService(dataSource);
-    }
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic().and()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated().and()
-                .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> routerFunction() {
-        return route(GET("/get"),
-                req -> ok().body(Map.of("Access", "Allowed")));
     }
 }
---
+++ src/main/java/org/bot0ff/model/Status.java
@@ -0,0 +1,5 @@
+package org.bot0ff.model;
+
+public enum Status {
+    ACTIVE, NOT_ACTIVE, DELETED
+}
